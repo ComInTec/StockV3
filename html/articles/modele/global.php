d91916dd39e60532c69a219ac0e124e591190a25
brahimmes <brahimmes@gmail.com>
2019-10-31 15:53:46 +0100

    reorganisation des fichiers

--- html/articles/modele/global.php
+++
@@ -1,7 +0,0 @@
-    <?php
-    ob_start();
-//    session_start();
-    $error = "Problem connecting";
-    $link = mysqli_connect('localhost','root','') or die($error);
-    mysqli_select_db($link,'dap2') or die($error);
-    ?>
--- html/fournisseurs/design/focus.js
+++
@@ -1,6 +0,0 @@
-    $(':input').focusin(function() {
-     $(this).css('background-color' , 'yellow');
-    });
-    $(':input').blur(function() {
-        $(this).css('background-color' , '#fff');
-    });
